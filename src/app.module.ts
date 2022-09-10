21c1f62702822ea91a8d130f43dca66a91d0166d
Pawel <pawel.sioda@gmail.com>
2022-09-10 21:52:28 +0200

    connect to database using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './typeorm.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService })],
   controllers: [AppController],
   providers: [AppService],
 })
