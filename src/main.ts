d317e55aa5edba08addb68ec876bd8369181c80c
Pawel <pawel.sioda@gmail.com>
2022-09-10 21:14:12 +0200

    finish basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const port: string = process.env.PORT;
+  await app.listen(port, () => {
+    console.log(`[Nest] Server is running at http://localhost:${port}`);
+  });
 }
 bootstrap();
